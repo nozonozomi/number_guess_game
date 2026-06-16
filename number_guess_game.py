e3f4d293b6549d356129a966dd8de8f6d46419e0
[nozomi] <[nozomi.belltree@gmail.com]>
2026-06-16 18:19:59 +0900

    add attempt limit

--- number_guess_game.py
+++ number_guess_game.py
@@ -1,19 +1,31 @@
 import random
 
 answer = random.randint(100, 999)
+max_attempts = 10
+attempts = 0
 
 print("数字当てゲーム！！")
 print("100から999の数字を当ててください！")
+print(f"回答できる回数は{max_attempts}回です！")
 
-guess = int(input("数字を入力してください: "))
+while attempts < max_attempts:
+    guess = int(input("数字を入力してください: "))
+    attempts += 1
 
-if guess == answer:
-    print("正解です！")
+    if guess == answer:
+        print("正解です！")
+        print(f"{attempts}回目で正解しました")
+        break
 
-else:
     print("不正解です")
 
     if guess < answer:
         print("もっと大きい数字です")
     else:
         print("もっと小さい数字です")
+
+    print(f"残り{max_attempts - attempts}回です")
+
+if guess != answer:
+    print("ゲームオーバーです")
+    print(f"正解は{answer}でした")
